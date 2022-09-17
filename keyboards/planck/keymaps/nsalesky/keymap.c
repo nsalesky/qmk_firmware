4132182796cc43fcc763ee830f932a5fc4410e76
Nick Salesky <nicksalesky@gmail.com>
2022-09-17 17:37:02 -0400

    More symmetry

--- keyboards/planck/keymaps/nsalesky/keymap.c
+++ keyboards/planck/keymaps/nsalesky/keymap.c
@@ -53,32 +53,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
  * |  Shift  |    Z    |    X    |    C    |    V    |    B    |    N    |    M    |    ,    |    .    |    /    | Shift ] |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * |   Alt   | <Leader>|  SUPER  |   NAV   |En(LOWER)|  Space  |  Space  |En(RAISE)|   Bspc  |   NAV   |   Alt   |   Alt   |
+ * |   Alt   |  Super  | <Leader>|   NAV   |En(LOWER)|  Space  |  Space  |En(RAISE)|   Bspc  |   NAV   |  Super  |   Alt   |
  * `-----------------------------------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     SFT_T(KC_TAB),         KC_Q,    KC_W,    KC_E,   KC_R,               KC_T,    KC_Y,     KC_U,               KC_I,    KC_O,   KC_P,    SFT_T(KC_LBRC),
     CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,   KC_F,               KC_G,    KC_H,     KC_J,               KC_K,    KC_L,   KC_SCLN, CTL_T(KC_QUOT),
     KC_LSFT,        KC_Z,    KC_X,    KC_C,   KC_V,               KC_B,    KC_N,     KC_M,               KC_COMM, KC_DOT, KC_SLSH, SFT_T(KC_RBRC),
-    KC_LALT,        KC_LEAD, KC_LGUI, NAV,    LT(LOWER, KC_ENT),  KC_SPC,  KC_SPC,   LT(RAISE, KC_ENT),  KC_BSPC, NAV,    KC_LALT, KC_LALT
+    KC_LALT,        KC_LGUI, KC_LEAD, NAV,    LT(LOWER, KC_ENT),  KC_SPC,  KC_SPC,   LT(RAISE, KC_ENT),  KC_BSPC, NAV,    KC_LGUI, KC_LALT
 ),
 
 /* Qwerty (Mac mode)
  * ,-----------------------------------------------------------------------------------------------------------------------.
  * |Shift Tab|    Q    |    W    |    E    |    R    |    T    |    Y    |    U    |    I    |    O    |    P    | Shift [ |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * | Ctl(Esc)|    A    |    S    |    D    |    F    |    G    |    H    |    J    |    K    |    L    |    ;    | Ctl(')  |
+ * | Sup(Esc)|    A    |    S    |    D    |    F    |    G    |    H    |    J    |    K    |    L    |    ;    | Sup(')  |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
  * |  Shift  |    Z    |    X    |    C    |    V    |    B    |    N    |    M    |    ,    |    .    |    /    | Shift ] |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * |   Alt   | <Leader>|  SUPER  |   NAV   |En(LOWER)|  Space  |  Space  |En(RAISE)|   Bspc  |   NAV   |   Alt   |   Alt   |
+ * |   Alt   |   Ctl   | <Leader>|   NAV   |En(LOWER)|  Space  |  Space  |En(RAISE)|   Bspc  |   NAV   |   Ctl   |   Alt   |
  * `-----------------------------------------------------------------------------------------------------------------------'
  */
 [_MAC] = LAYOUT_planck_grid(
     SFT_T(KC_TAB),         KC_Q,    KC_W,    KC_E,   KC_R,               KC_T,    KC_Y,     KC_U,               KC_I,    KC_O,   KC_P,    SFT_T(KC_LBRC),
-    GUI_T(KC_ESC),  KC_A,    KC_S,    KC_D,   KC_F,               KC_G,    KC_H,     KC_J,               KC_K,    KC_L,   KC_SCLN, CTL_T(KC_QUOT),
+    GUI_T(KC_ESC),  KC_A,    KC_S,    KC_D,   KC_F,               KC_G,    KC_H,     KC_J,               KC_K,    KC_L,   KC_SCLN, GUI_T(KC_QUOT),
     KC_LSFT,        KC_Z,    KC_X,    KC_C,   KC_V,               KC_B,    KC_N,     KC_M,               KC_COMM, KC_DOT, KC_SLSH, SFT_T(KC_RBRC),
-    KC_LALT,        KC_LEAD, KC_LCTL, NAV,    LT(LOWER, KC_ENT),  KC_SPC,  KC_SPC,   LT(RAISE, KC_ENT),  KC_BSPC, NAV,    KC_LALT, KC_LALT
+    KC_LALT,        KC_LCTL, KC_LGUI, NAV,    LT(LOWER, KC_ENT),  KC_SPC,  KC_SPC,   LT(RAISE, KC_ENT),  KC_BSPC, NAV,    KC_LCTL, KC_LALT
 ),
 
 /* Lower
