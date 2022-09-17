830f2af9923eafe8e7d9eb3f7bf1acb3396b3a7e
Nick Salesky <nicksalesky@gmail.com>
2022-09-17 17:26:13 -0400

    Optimized keymap with more modifers on right side

--- keyboards/planck/keymaps/nsalesky/keymap.c
+++ keyboards/planck/keymaps/nsalesky/keymap.c
@@ -47,38 +47,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------------------------------------------.
- * |   Tab   |    Q    |    W    |    E    |    R    |    T    |    Y    |    U    |    I    |    O    |    P    |    [    |
+ * |   Tab   |    Q    |    W    |    E    |    R    |    T    |    Y    |    U    |    I    |    O    |    P    | Shift [ |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * | Ctl(Esc)|    A    |    S    |    D    |    F    |    G    |    H    |    J    |    K    |    L    |    ;    |    '    |
+ * | Ctl(Esc)|    A    |    S    |    D    |    F    |    G    |    H    |    J    |    K    |    L    |    ;    | Ctl(')  |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * |  Shift  |    Z    |    X    |    C    |    V    |    B    |    N    |    M    |    ,    |    .    |    /    |    ]    |
+ * |  Shift  |    Z    |    X    |    C    |    V    |    B    |    N    |    M    |    ,    |    .    |    /    | Shift ] |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * |   Alt   |  Leader |  SUPER  |   NAV   |En(LOWER)|  Space  |  Space  |En(RAISE)|   Bspc  |   NAV   |   Alt   |  Shift  |
+ * |   Alt   | <Leader>|  SUPER  |   NAV   |En(LOWER)|  Space  |  Space  |En(RAISE)|   Bspc  |   NAV   |   Alt   |   Alt   |
  * `-----------------------------------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,         KC_Q,    KC_W,    KC_E,   KC_R,               KC_T,    KC_Y,     KC_U,               KC_I,    KC_O,   KC_P,    KC_LBRC,
-    CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,   KC_F,               KC_G,    KC_H,     KC_J,               KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-    KC_LSFT,        KC_Z,    KC_X,    KC_C,   KC_V,               KC_B,    KC_N,     KC_M,               KC_COMM, KC_DOT, KC_SLSH, KC_RBRC,
-    KC_LALT,        KC_LEAD, KC_LGUI, NAV,    LT(LOWER, KC_ENT),  KC_SPC,  KC_SPC,   LT(RAISE, KC_ENT),  KC_BSPC, NAV,    KC_LALT, KC_LSFT
+    KC_TAB,         KC_Q,    KC_W,    KC_E,   KC_R,               KC_T,    KC_Y,     KC_U,               KC_I,    KC_O,   KC_P,    SFT_T(KC_LBRC),
+    CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,   KC_F,               KC_G,    KC_H,     KC_J,               KC_K,    KC_L,   KC_SCLN, CTL_T(KC_QUOT),
+    KC_LSFT,        KC_Z,    KC_X,    KC_C,   KC_V,               KC_B,    KC_N,     KC_M,               KC_COMM, KC_DOT, KC_SLSH, SFT_T(KC_RBRC),
+    KC_LALT,        KC_LEAD, KC_LGUI, NAV,    LT(LOWER, KC_ENT),  KC_SPC,  KC_SPC,   LT(RAISE, KC_ENT),  KC_BSPC, NAV,    KC_LALT, KC_LALT
 ),
 
-/* Qwerty
+/* Qwerty (Mac mode)
  * ,-----------------------------------------------------------------------------------------------------------------------.
- * |   Tab   |    Q    |    W    |    E    |    R    |    T    |    Y    |    U    |    I    |    O    |    P    |    [    |
+ * |   Tab   |    Q    |    W    |    E    |    R    |    T    |    Y    |    U    |    I    |    O    |    P    | Shift [ |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * | Ctl(Esc)|    A    |    S    |    D    |    F    |    G    |    H    |    J    |    K    |    L    |    ;    |    '    |
+ * | Ctl(Esc)|    A    |    S    |    D    |    F    |    G    |    H    |    J    |    K    |    L    |    ;    | Ctl(')  |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * |  Shift  |    Z    |    X    |    C    |    V    |    B    |    N    |    M    |    ,    |    .    |    /    |    ]    |
+ * |  Shift  |    Z    |    X    |    C    |    V    |    B    |    N    |    M    |    ,    |    .    |    /    | Shift ] |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * | <Leader>|   Alt   |  SUPER  |   NAV   |En(LOWER)|  Space  |  Space  |En(RAISE)|   Bspc  |   NAV   |   Alt   |  Shift  |
+ * |   Alt   | <Leader>|  SUPER  |   NAV   |En(LOWER)|  Space  |  Space  |En(RAISE)|   Bspc  |   NAV   |   Alt   |   Alt   |
  * `-----------------------------------------------------------------------------------------------------------------------'
  */
 [_MAC] = LAYOUT_planck_grid(
-    KC_TAB,         KC_Q,    KC_W,    KC_E,   KC_R,               KC_T,    KC_Y,     KC_U,               KC_I,    KC_O,   KC_P,    KC_LBRC,
-    GUI_T(KC_ESC),  KC_A,    KC_S,    KC_D,   KC_F,               KC_G,    KC_H,     KC_J,               KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-    KC_LSFT,        KC_Z,    KC_X,    KC_C,   KC_V,               KC_B,    KC_N,     KC_M,               KC_COMM, KC_DOT, KC_SLSH, KC_RBRC,
-    KC_LEAD,        KC_LALT, KC_LCTL, NAV,    LT(LOWER, KC_ENT),  KC_SPC,  KC_SPC,   LT(RAISE, KC_ENT),  KC_BSPC, NAV,    KC_LALT, KC_LSFT
+    KC_TAB,         KC_Q,    KC_W,    KC_E,   KC_R,               KC_T,    KC_Y,     KC_U,               KC_I,    KC_O,   KC_P,    SFT_T(KC_LBRC),
+    GUI_T(KC_ESC),  KC_A,    KC_S,    KC_D,   KC_F,               KC_G,    KC_H,     KC_J,               KC_K,    KC_L,   KC_SCLN, CTL_T(KC_QUOT),
+    KC_LSFT,        KC_Z,    KC_X,    KC_C,   KC_V,               KC_B,    KC_N,     KC_M,               KC_COMM, KC_DOT, KC_SLSH, SFT_T(KC_RBRC),
+    KC_LALT,        KC_LEAD, KC_LCTL, NAV,    LT(LOWER, KC_ENT),  KC_SPC,  KC_SPC,   LT(RAISE, KC_ENT),  KC_BSPC, NAV,    KC_LALT, KC_LALT
 ),
 
 /* Lower
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |WN/LNX| Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | MAC  |      |MUSmod|Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|Plover|      |
+ * | MAC  |      |MUSmod|Aud on|Audoff|AGnorm|AGswap|Qwerty|      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
